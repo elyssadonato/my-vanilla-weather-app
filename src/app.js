a8ebe3e89a33bc61cb6d4d72fbf5ee035163b26a
elyssadonato <142195541+elyssadonato@users.noreply.github.com>
2023-08-17 18:12:46 -0700

    Updating JavaScript to show current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let weatherDescription = document.querySelector("#weather-description");
+  weatherDescription.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "bc74ea37ddf606c1c06f602d803cbb3a";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+axios.get(apiUrl).then(displayTemperature);
